787b92c1dcb20b9380002735089e840d0041196a
yoneison <yoneisonbayona@gmail.com>
2024-03-28 17:30:31 -0500

    update url

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [userUpdate, setUserUpdate] = useState();
   const [isFormClose, setIsFormClose] = useState(true);
   const [notification, setNotification] = useState(null);
-  const baseUrl = 'https://users-crud.academlo.tech';
+  const baseUrl = 'https://api-user-crud-test.onrender.com/api/v1';
   const [users, getUsers, createUser, deleteUser, updateUser] = useFetch(baseUrl);
 
   useEffect(() => {
